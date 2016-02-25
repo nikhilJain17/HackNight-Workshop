e7d584c8c5508e8516a4824041e9c5a2f90773e3
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-02-24 23:55:59 -0500

    added thng to have get page

--- src/main/BrowserWindow.java
+++ src/main/BrowserWindow.java
@@ -1,6 +1,13 @@
 package main;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.swing.*;
 
 public class BrowserWindow {
@@ -33,7 +40,8 @@ public class BrowserWindow {
 		// build displayPane
 		displayPane = new JEditorPane(); 
 		displayPane.setContentType("text/html");
-		displayPane.setPreferredSize(new Dimension(500, 500));
+		displayPane.setPreferredSize(new Dimension(666, 666));
+		
 		
 		// use SpringLayout to position the GUI components
 		// create rules for adding stuff to the frame
@@ -41,13 +49,49 @@ public class BrowserWindow {
 		// put 5 px of room from left edge of search bar to left edge of frame
 		layoutManager.putConstraint(SpringLayout.WEST, searchBar, 5, SpringLayout.WEST, frame);
 		
+		// put 5 px of room from right of button to right of frame
+		layoutManager.putConstraint(SpringLayout.EAST, goButton, 5, SpringLayout.EAST, frame);
+		
+		// put 5 px of room from top of displayPane and  bottom of search bar
+		layoutManager.putConstraint(SpringLayout.NORTH, displayPane, 5, SpringLayout.SOUTH, searchBar);
+		
+		// put 5 px of room from left of displayPane and left of frame
+		layoutManager.putConstraint(SpringLayout.WEST, displayPane, 5, SpringLayout.WEST, frame);
+
+		
+		// set on click listener for button
+		// if you click the button, it goes to the URL typed in the search bar
+		// sends a GET request to the server
+		// gets the output content
+		// displays it in the displayPane
+		goButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button clicked");
+				// TODO Auto-generated method stub
+				try {
+					System.out.println("fetching html");
+//					String html = getHTML(searchBar.getText());
+//					displayPane.setText(html);
+//					System.out.println(html);
+					displayPane.setPage(searchBar.getText());
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+		});
+		
+		
 		
 		// add the components to the frame
 		frame.add(goButton);
 		frame.add(searchBar);
 		frame.add(displayPane);
 		
-		// display the frame
+		// display the GUI
 		goButton.setVisible(true);
 		searchBar.setVisible(true);
 		displayPane.setVisible(true);
@@ -55,7 +99,22 @@ public class BrowserWindow {
 		frame.pack();
 		frame.setVisible(true);
 		
+		
 	} // end of constructor
+	
+	private static String getHTML(String urlToRead) throws Exception {
+	      StringBuilder result = new StringBuilder();
+	      URL url = new URL(urlToRead);
+	      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	      conn.setRequestMethod("GET");
+	      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	      String line;
+	      while ((line = rd.readLine()) != null) {
+	         result.append(line);
+	      }
+	      rd.close();
+	      return result.toString();
+	   }
 	
 	public static void main(String[] args) {
 		
