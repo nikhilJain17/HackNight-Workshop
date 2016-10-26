cfedc907cfd7923a218b7ad4150b79199e6ee9c1
vinija <vinija@gmail.com>
2016-10-26 11:10:40 -0400

    Update BrowserWindow.java

--- src/main/BrowserWindow.java
+++ src/main/BrowserWindow.java
@@ -138,21 +138,21 @@ public class BrowserWindow {
 	
 	// The hard way: send a get request manually 
 	
-//	
-//	private static String getHTML(String urlToRead) throws Exception {
-//	      StringBuilder result = new StringBuilder();
-//	      URL url = new URL(urlToRead);
-//	      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//	      conn.setRequestMethod("GET");
-//	      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//	      String line;
-//	      while ((line = rd.readLine()) != null) {
-//	         result.append(line);
-//	      }
-//	      rd.close();
-//	      return result.toString();
-//	   }
-//	
+	
+	private static String getHTML(String urlToRead) throws Exception {
+	      StringBuilder result = new StringBuilder();
+	      URL url = new URL(urlToRead);
+	      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	      conn.setRequestMethod("GET");
+	      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	      String line;
+	      while ((line = rd.readLine()) != null) {
+	         result.append(line);
+	      }
+	      rd.close();
+	      return result.toString();
+	   }
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
